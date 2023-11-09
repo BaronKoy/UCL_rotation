4462b7d1b208b89955001e086946598b3579f96e
BaronKoy <bkero1991@gmail.com>
2023-11-09 13:11:53 +0000

    update for gene2snp

--- gene2snp.py
+++ gene2snp.py
@@ -10,11 +10,11 @@ url = f'https://rest.ensembl.org/overlap/id/{entrez}?content-type=application/js
 rurl = requests.get(url)
 outurl = rurl.json()
 #Only use the line below should you wish to printout the gene name to the top of the VEP output file (though this then cannot be used as an input)
-#print('Gene:', outurl[0]['external_name']) 
+print('Gene:', outurl[0]['external_name'])
 genename = outurl[0]['external_name']
 
 # Parse API output for VEP input entries (chromsome, start, end, allele, strand, identifier) and create file for VEP input
-with open('out.txt', 'w') as f:
+with open('default_input_out.txt', 'w') as f:
     for x in outurl:
         if x['strand'] == 1:
             x['strand'] = '+'
@@ -25,9 +25,16 @@ with open('out.txt', 'w') as f:
             alle2 = (x['alleles'][1])
             final_allele = alle1 + '/' + alle2
             print(x['seq_region_name'], x['start'], x['end'], final_allele, x['strand'], x['id'], sep='\t', file=f)
-print(genename, 'vep output file created')
-print('please run ./vep -i out.txt -o vep_output')
+print('vep default input file successfully created')
+print('For default input: please run vep -i default_input_out.txt -o vep_output.txt --cache ~/.vep/homo_sapiens')
 
 #VEP can also use variant identifier as an input (though this method is not recommended due to processing speed)
-#The option below can be used if you want to use the identifier but keep in the mind the note from the line above
-#print(x['id'])
+#The option below will use the identifier but keep in the mind the note from the line above
+
+with open('rsID_input_out.txt', 'w') as z:
+    for y in outurl:
+        if y['id'].startswith('rs'):
+            rsid = y['id']
+            print(rsid, file=z)
+print('vep rs ID identifier input file successfully created')
+print('For rs_ID input: please run vep -i rsID_input_out.txt -o vep_output.txt --cache ~/.vep/homo_sapiens')
